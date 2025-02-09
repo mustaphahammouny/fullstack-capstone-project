a76b69c41711d2fcf7f4e2eb3463181318b0ee4f
mustapha <mustapha.hammouny@gmail.com>
2025-02-09 15:23:00 -0500

    Search page

--- giftlink-frontend/src/components/Navbar/Navbar.js
+++ giftlink-frontend/src/components/Navbar/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
     return (
@@ -11,7 +12,10 @@ export default function Navbar() {
                         <a className="nav-link" href="/home.html">Home</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="/app">Gifts</a>
+                        <Link className="nav-link" href="/app">Gifts</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/app/search">Search</Link>
                     </li>
                 </ul>
             </div>
